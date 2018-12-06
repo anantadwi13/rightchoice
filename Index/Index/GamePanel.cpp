cb8cbf0627e57d524c04e5ed9b3ed45bc198f236
ranggakd <ranggakd@users.noreply.github.com>
2018-12-06 22:05:49 +0700

    Update game panel
    -penataan variabel agar mudah di assign
    -bar ada 2 buat penentu

--- Index/Index/GamePanel.cpp
+++ Index/Index/GamePanel.cpp
@@ -71,69 +71,122 @@ void GamePanel::OnPaint(wxPaintEvent &event)
 	wxPaintDC pdc(this);
 	ImageLoaderList::Node* tmp = this->img->GetFirst();
 
+	//variabel gambar
+	int posX, posY;
+
 	while (tmp)
 	{
 		ImageLoader *iml = tmp->GetData();
 		if (iml->loc == wxT("img//bg3.jpg"))
 			pdc.DrawBitmap(*iml->bitImage, wxPoint(0, 0), true);
 		else if (iml->loc == wxT("img//bar_otak.png"))
-		{
-			int posX = SwitchFrame::width / 5 - iml->bitImage->GetWidth() / 2;
-			int posY = SwitchFrame::height / 10 - iml->bitImage->GetHeight() / 2;
+		{	//draw bar otak
+			posX = SwitchFrame::width / 5 - iml->bitImage->GetWidth() / 2;
+			posY = SwitchFrame::height / 10 - iml->bitImage->GetHeight() / 2;
 			pdc.DrawBitmap(*iml->bitImage, wxPoint(posX, posY), true);
-			int xrect = SwitchFrame::width / 5 + 60;
-			int yrect = SwitchFrame::height / 10 - 20;
-			int width = 300;
-			//wxMessageOutputDebug().Printf("w %d", SwitchFrame::width / 2 - xrect - width);
-			pdc.SetPen(*wxGREEN_PEN);
-			pdc.SetBrush(*wxGREEN_BRUSH);
-			pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 40));
 		}
 		else if (iml->loc == wxT("img//bar_otot.png"))
-		{
-			int posX = SwitchFrame::width * 4 / 5 - iml->bitImage->GetWidth() / 2;
-			int posY = SwitchFrame::height / 10 - iml->bitImage->GetHeight() / 2;
+		{	//draw bar otot
+			posX = SwitchFrame::width / 2 + 54 - iml->bitImage->GetWidth() / 2;
+			//posX = SwitchFrame::width * 4 / 5 - iml->bitImage->GetWidth() / 2;
+			posY = SwitchFrame::height / 10 - iml->bitImage->GetHeight() / 2;
 			pdc.DrawBitmap(*iml->bitImage, wxPoint(posX, posY), true);
-			int xrect = SwitchFrame::width / 2 + 24;
-			int yrect = SwitchFrame::height / 10 - 20;
-			int width = 300;
-			pdc.SetPen(*wxGREEN_PEN);
-			pdc.SetBrush(*wxGREEN_BRUSH);
-			pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 40));
 		}
 		else if (iml->loc == wxT("img//bar_orang.png"))
-		{
-			int posX = SwitchFrame::width / 5 - iml->bitImage->GetWidth() / 2;
-			int posY = SwitchFrame::height / 10 + 80 - iml->bitImage->GetHeight() / 2;
+		{	//draw bar orang
+			posX = SwitchFrame::width / 5 - iml->bitImage->GetWidth() / 2;
+			posY = SwitchFrame::height / 10 + 80 - iml->bitImage->GetHeight() / 2;
 			pdc.DrawBitmap(*iml->bitImage, wxPoint(posX, posY), true);
-			int xrect = SwitchFrame::width / 5 + 60;
-			int yrect = SwitchFrame::height / 10 + 60;
-			int width = 300;
-			//wxMessageOutputDebug().Printf("w %d", SwitchFrame::width / 2 - xrect - width);
-			pdc.SetPen(*wxGREEN_PEN);
-			pdc.SetBrush(*wxGREEN_BRUSH);
-			pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 40));
 		}
 		else if (iml->loc == wxT("img//bar_uang.png"))
-		{
-			int posX = SwitchFrame::width * 4 / 5 - iml->bitImage->GetWidth() / 2;
-			int posY = SwitchFrame::height / 10 + 80 - iml->bitImage->GetHeight() / 2;
+		{	//draw bar uang
+			posX = SwitchFrame::width / 2 + 54 - iml->bitImage->GetWidth() / 2;
+			//posX = SwitchFrame::width * 4 / 5 - iml->bitImage->GetWidth() / 2;
+			posY = SwitchFrame::height / 10 + 80 - iml->bitImage->GetHeight() / 2;
 			pdc.DrawBitmap(*iml->bitImage, wxPoint(posX, posY), true);
-			int xrect = SwitchFrame::width / 2 + 24;
-			int yrect = SwitchFrame::height / 10 + 60;
-			int width = 300;
-			pdc.SetPen(*wxGREEN_PEN);
-			pdc.SetBrush(*wxGREEN_BRUSH);
-			pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 40));
 		}
 		else if (iml->loc == wxT("img//fig_dosen.png"))
 		{
-			int posX = SwitchFrame::width / 2 - iml->bitImage->GetWidth() / 2;
-			int posY = SwitchFrame::height * 3 / 5 - iml->bitImage->GetHeight() / 2;
+			posX = SwitchFrame::width / 2 - iml->bitImage->GetWidth() / 2;
+			posY = SwitchFrame::height * 3 / 5 - iml->bitImage->GetHeight() / 2;
 			pdc.DrawBitmap(*iml->bitImage, wxPoint(posX, posY), true);
 		}
 		tmp = tmp->GetNext();
 	}
+	//variable drawing bar dan gauge
+	int xrect, yrect, width;
+
+	//otak rect gauge (100% fixed)
+	xrect = SwitchFrame::width / 5 + 60;
+	yrect = SwitchFrame::height / 10 - 20;
+	width = 300;
+	//wxMessageOutputDebug().Printf("w %d", SwitchFrame::width / 2 - xrect - width);
+	pdc.SetPen(*wxGREY_PEN);
+	pdc.SetBrush(*wxGREY_BRUSH);
+	pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 40));
+	//otak rect bar
+	width = 150;		//separuh dari gauge
+	pdc.SetPen(*wxGREEN_PEN);
+	pdc.SetBrush(*wxGREEN_BRUSH);
+	pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 40));
+
+	//otot rect gauge (100% fixed)
+	xrect = SwitchFrame::width / 2 + 114;
+	yrect = SwitchFrame::height / 10 - 20;
+	width = 300;
+	pdc.SetPen(*wxGREY_PEN);
+	pdc.SetBrush(*wxGREY_BRUSH);
+	pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 40));
+	//otot rect bar
+	width = 150;		//separuh dari gauge
+	pdc.SetPen(*wxGREEN_PEN);
+	pdc.SetBrush(*wxGREEN_BRUSH);
+	pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 40));
+
+	//orang rect gauge (100% fixed)
+	 xrect = SwitchFrame::width / 5 + 60;
+	 yrect = SwitchFrame::height / 10 + 60;
+	width = 300;
+	//wxMessageOutputDebug().Printf("w %d", SwitchFrame::width / 2 - xrect - width);
+	pdc.SetPen(*wxGREY_PEN);
+	pdc.SetBrush(*wxGREY_BRUSH);
+	pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 40));
+	//orang rect bar
+	width = 150;		//separuh dari gauge
+	//wxMessageOutputDebug().Printf("w %d", SwitchFrame::width / 2 - xrect - width);
+	pdc.SetPen(*wxGREEN_PEN);
+	pdc.SetBrush(*wxGREEN_BRUSH);
+	pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 40));
+
+	//uang rect gauge (100% fixed)
+	xrect = SwitchFrame::width / 2 + 114;
+	yrect = SwitchFrame::height / 10 + 60;
+	width = 300;
+	pdc.SetPen(*wxGREY_PEN);
+	pdc.SetBrush(*wxGREY_BRUSH);
+	pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 40));
+	//uang rect bar
+	width = 150;		//separuh dari gauge
+	pdc.SetPen(*wxGREEN_PEN);
+	pdc.SetBrush(*wxGREEN_BRUSH);
+	pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 40));
+
+	//problem rect
+	xrect = SwitchFrame::width / 2 - 150;
+	yrect = SwitchFrame::height * 3 / 5 - 240;
+	width = 300;
+	pdc.SetPen(*wxWHITE_PEN);
+	pdc.SetBrush(*wxWHITE_BRUSH);
+	pdc.DrawRectangle(wxPoint(xrect, yrect), wxSize(width, 80));
+	//problem string (6 pixel ~ 1 char)
+	wxString prob = wxT(
+		"Ini problem yang harus kamu selesaikan dalam waktu\nkurang dari "
+	"seminggu. Dan dihari Senin kau harus mendemokannya. Apakah kamu siap?");
+	pdc.DrawText(prob, wxPoint(xrect + 10, yrect + 10));
+	wxString p = wxT("Ini problem yang harus kamu selesaikan dalam waktu");
+	//wxMessageOutputDebug().Printf("p %d", p.Length());
+	//p.GetIterForNthChar(2);
+	
 	//footer current score
 	pdc.SetPen(*wxBLUE_PEN);
 	pdc.SetBrush(*wxBLUE_BRUSH);
